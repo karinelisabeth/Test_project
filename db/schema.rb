77fd2ae79ed936a05ef07ff01d89b1fde485bcf9
Elisabeth Modin <elisabethmodin@gmail.com>
2015-06-20 10:26:09 +0100

    added devise authenticationn. added organisation and membership

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522113828) do
+ActiveRecord::Schema.define(version: 20150619172535) do
+
+  create_table "memberships", force: :cascade do |t|
+    t.string   "invitation_code", limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "activated_at"
+    t.datetime "deactivated_at"
+    t.integer  "organisation_id", limit: 4
+    t.integer  "user_id",         limit: 4
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "organisations", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "product_categories", force: :cascade do |t|
     t.string   "cat_name",   limit: 255
@@ -25,5 +42,23 @@ ActiveRecord::Schema.define(version: 20150522113828) do
     t.datetime "updated_at",                      null: false
     t.integer  "product_category_id", limit: 4
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
