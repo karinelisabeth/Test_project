2f8ee85cbab1a6ec4c3fc21c2a591ee5a4484371
Elisabeth Modin <elisabethmodin@gmail.com>
2015-07-23 19:03:07 +0100

    added roles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717152752) do
+ActiveRecord::Schema.define(version: 20150723180133) do
 
   create_table "memberships", force: :cascade do |t|
     t.string   "invitation_code", limit: 255
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20150717152752) do
     t.datetime "updated_at",                      null: false
     t.integer  "product_category_id", limit: 4
     t.integer  "organisation_id",     limit: 4
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "role",            limit: 255
+    t.integer  "organisation_id", limit: 4
+    t.integer  "user_id",         limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "users", force: :cascade do |t|
