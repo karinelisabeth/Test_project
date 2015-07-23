fdba05c6a70051be974e3cfb6b2820d0e12d65fa
Elisabeth Modin <elisabethmodin@gmail.com>
2015-07-23 11:25:09 +0100

    added scope, only the role 'account_owner' can see all products

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,19 +1,25 @@
-class ProductPolicy
-  attr_reader :current_user, :model
 
-  def initialize(current_user, model)
-    @current_user = current_user
-    @product = model
+class ProductPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    def resolve
+      if user.account_owner?
+        scope.all
+      else
+        scope.where(:id => 0)
+      end
+    end
   end
 
+
   def permitted_attributes
-    puts "permitted"
-    if @current_user.account_owner?
+    if user.account_owner?
       [:product_name,  :product_category_id]
     else
       [:product_category_id]
     end
   end
+
 
   #(:product_name, :product_category_id,
   #:product_category_attributes => [:id, :cat_name]
